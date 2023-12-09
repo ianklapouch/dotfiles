c127b92f4096f6bfdbee9a74f3c12c90a3968273
Ian Gabriel Klapouch <ianklapouch@gmail.com>
2023-12-09 12:50:33 -0300

    neovim files

---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,13 @@
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+
+vim.opt.wrap = false
+
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+
+vim.opt.ignorecase = true -- Case insensitive searching UNLESS /C or capital in search
+
+vim.opt.foldmethod = "manual"
---
+++ .config/nvim/lua/plugins/coding.lua
@@ -0,0 +1,11 @@
+return {
+  -- Better increase/descrease
+  {
+    "monaqa/dial.nvim",
+    -- stylua: ignore
+    keys = {
+      { "<C-a>", function() return require("dial.map").inc_normal() end, expr = true, desc = "Increment" },
+      { "<C-x>", function() return require("dial.map").dec_normal() end, expr = true, desc = "Decrement" },
+    },
+  },
+}
