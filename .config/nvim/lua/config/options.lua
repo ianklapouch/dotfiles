d51ff919f0178716f55959963bfd63fa38c6f9f6
Ian Gabriel Klapouch <ianklapouch@gmail.com>
2024-11-17 17:59:51 -0300

    Update options.lua

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -16,12 +16,21 @@ vim.opt.foldmethod = "manual"
 vim.opt.spell = false
 
 -- Neovide
-vim.opt.guifont = "JetBrainsMono Nerd Font:h14"
--- vim.opt.guifont = "Monaspace Krypton:h14"
-vim.g.neovide_scroll_animation_length = 0
+if vim.g.neovide then
+  -- Disable animations
+  vim.g.neovide_position_animation_length = 0
+  vim.g.neovide_cursor_animation_length = 0.00
+  vim.g.neovide_cursor_trail_size = 0
+  vim.g.neovide_cursor_animate_in_insert_mode = false
+  vim.g.neovide_cursor_animate_command_line = false
+  vim.g.neovide_scroll_animation_far_lines = 0
+  vim.g.neovide_scroll_animation_length = 0.00
+  -- Font
+  vim.o.guifont = "JetBrainsMono Nerd Font"
+end
+
 
 -- Power Shell
-
 vim.opt.shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell"
 vim.opt.shellcmdflag =
   "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
